db1ef0843e3a569a743567464ff878d4b4656c94
DeepakFromZeus <manedeep2001@gmail.com>
2024-10-26 22:26:35 +0530

    resolve the knexfile issue

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,5 +1,5 @@
 import knex from 'knex';
-import knexConfig from '../../knexfile'; // Adjust path to knexfile
+import knexConfig from '../knexfile'; // Adjust path to knexfile
 
 const db = knex(knexConfig);
 
